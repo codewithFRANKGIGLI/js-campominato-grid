f20e19cedd9d2e1fbcc30b642d19befd78345d2f
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-03-18 14:29:10 +0100

    feat: correzione

--- js/script.js
+++ js/script.js
@@ -1,50 +1,57 @@
 // Consegna
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 // creo l'evento in ascolto del click per rimuovere d-none da grid:
+const mainGrid = document.querySelector('#grid');
 const playBtn = document.querySelector('#playBtn');
-playBtn.addEventListener('click', function() {
-    const displayGrid  = document.querySelector('#grid');
-    displayGrid.classList.remove('d-none');
-});
+playBtn.addEventListener('click', createNewGame);
 
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // per 100 volte creare un quadratino e ogni quadratino
 // lo aggiungo a grid
-    const mainGrid = document.querySelector('#grid');
-    for(let i = 1; i <= 100; i++) {
-        const newSquare = generateSquare(i);    // chiamo la funzione
-        mainGrid.append(newSquare);
+function createNewGame() {
+    mainGrid.innerHTML = '';
+    const level = document.querySelector('#level').value;
+    let numberOfSquares;
+    let numberOfCellsPerRow;
+        if (level === 'easy') {
+            numberOfSquares = 100;
+            numberOfCellsPerRow = 10;
+        } else if(level === 'normal') {
+            numberOfSquares = 81;
+            numberOfCellsPerRow = 9;
+        } else if(level === 'hard') {
+            numberOfSquares = 49;
+            numberOfCellsPerRow = 7;
+        }
+        console.log(numberOfSquares, numberOfCellsPerRow);
+    for(let i = 1; i <= numberOfSquares; i++) {
+        const thisNumber = i;
+
+        const square = generateSquare(thisNumber, numberOfCellsPerRow);    // chiamo la funzione
+        square.addEventListener('click', function() {
+            this.classList.add('my-event-bg');
+            setTimeout(function() {
+                alert(`Hai cliccato:  ${thisNumber}`);
+            }, 0);
+        });
+        mainGrid.append(square);
     }
+
+}
 
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
     // number -> numero che rappresenta un numero
     // return: elemento del dom che rappresenta un quadrato
 
-    function generateSquare(number) {
-        const newSquare = document.createElement('div');
-        newSquare.classList.add('square');
-        newSquare.innerHTML = `<span>${number}</span>`;
-      
-        // Gestione del click su ogni quadrato
-        newSquare.addEventListener('click', function() {
-            // Cambia il bg dell'elemento cliccato
-            this.style.backgroundColor = 'lightskyblue';
-        
-            // Mostra l'alert togliendo il delay, senza questo prima verrebbe l'alert e poi il cambio del bg
-            setTimeout(function() {
-                alert(`Hai cliccato:  ${number}`);
-            }, 0);
-        });
-      
-        return newSquare;
-    }
+function generateSquare(number, cellsPerRow) {
+    const newSquare = document.createElement('div');
+    newSquare.classList.add('square');
+    newSquare.innerHTML = number; 
+    newSquare.style.width = `calc(100% / ${cellsPerRow})`;
+    newSquare.style.height = `calc(100% / ${cellsPerRow})`;
 
-// Bonus
-// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-// Consigli del giorno:  :party_wizard:
-// Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-
+    
+    
+    return newSquare;
+}
